71c55345c00a29203b67d9058d2556f7003aec67
viditpokhral <viditpokhral@gmail.com>
2024-03-17 18:49:55 +0530

    edit move

--- src/com/chess/engine/board/Move.java
+++ src/com/chess/engine/board/Move.java
@@ -218,7 +218,7 @@ public abstract class Move {
                     return move;
                 }
             }
-            return NULL_MOVE;
+            return NULL_MOVE ;
         }
     }
 }
